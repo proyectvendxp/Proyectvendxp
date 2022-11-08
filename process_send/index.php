66b1916135d387823442fdc64e6906868b8e2d2e
LUIS_9799 <68512719+LUIS9799@users.noreply.github.com>
2022-11-07 19:46:59 -0500

    update

--- process_send/index.php
+++ process_send/index.php
@@ -6,6 +6,6 @@ if(!isset($_SESSION['UserData']['Username'])){
 	exit;
 }
   ?>
-  <h1>Test</h1>
-<footer>Creado por Luis-Net (Luis9799) https://luisweb.cf</footer>
+  <h1>uwu</h1>
+<footer>Created and Built by: Luis-Net (Luis9799) https://luisweb.cf</footer>
 <a href="\return">Return to main menu.</a>

3a997f794cbe42afee72f09f852990a66221b624
LUIS_9799 <68512719+LUIS9799@users.noreply.github.com>
2022-11-07 19:54:15 -0500

    Update legal

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
-echo Iniciando..
+echo Starting...
 sleep 1.5
 cd Site
 sleep 1
@@ -16,11 +16,16 @@ echo "  ░ ░    ░░░ ░ ░  ▒ ░░  ░  ░     ░   ░ ░    
 echo "    ░  ░   ░      ░        ░           ░    ░  ░        ";
 echo "                                                        ";
 sleep 5
-echo Ubicando Carpeta...
-clear
-echo Creado por Luis9799 
-sleep 1
-echo Iniciando webserver..
+echo "Looking for folder"
+echo "Created by: Luis9799 for Southestearn Lousiana University"
+echo "Copyright all rights reserved 2022, MIT LICENSE"
+echo "You're free to use this software for your own, you can NOT distributed by your name and obtain earnings"
+echo "Twitter: @yt1luiscraft"
+echo "Discord: Luis!#5902"
+echo "Telegram: @L9799_1"
+echo "Web: https://luisweb.cf"
+sleep 3
+echo Starting webserver...
 sleep 1
 echo WebServer iniciado en el puerto 8080
 sleep 2

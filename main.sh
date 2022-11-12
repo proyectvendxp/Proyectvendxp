7ad56c090a475f57230981cbadf5e0376a05210b
LUIS_9799 <68512719+LUIS9799@users.noreply.github.com>
2022-11-12 13:01:33 -0500

    Legal

--- main.sh
+++ main.sh
@@ -19,7 +19,7 @@ sleep 5
 echo "Looking for folder"
 echo "Created by: Luis9799 for Southestearn Lousiana University"
 echo "Copyright all rights reserved 2022, MIT LICENSE"
-echo "You're free to use this software for your own, you can NOT distributed by your name and obtain earnings"
+echo "You're free to use this software for your own, you can NOT distributed by your name and obtain earnings, otherwise, local law will be applied."
 echo "Twitter: @yt1luiscraft"
 echo "Discord: Luis!#5902"
 echo "Telegram: @L9799"

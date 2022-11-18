9d2ad6ba701ef27d3534bdbc7d4bd5e8704f570b
LUIS_9799 <68512719+LUIS9799@users.noreply.github.com>
2022-11-18 11:54:49 -0500

    Message

--- process_send/index.php
+++ process_send/index.php
@@ -6,6 +6,7 @@ if(!isset($_SESSION['UserData']['Username'])){
 	exit;
 }
   ?>
+<h2>You're not able to see internal system files.</h2>
   <h1>uwu</h1>
 <footer>Created and Built by: Luis-Net (Luis9799) https://luisweb.cf</footer>
 <a href="\return">Return to main menu.</a>

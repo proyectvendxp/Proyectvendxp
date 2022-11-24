1b91d8d28fa063f3e549d81d18759820882961e8
LUIS_9799 <68512719+LUIS9799@users.noreply.github.com>
2022-11-24 12:46:36 -0500

    💖💖

--- process_send/index.php
+++ process_send/index.php
@@ -12,5 +12,5 @@ if(!isset($_SESSION['UserData']['Username'])){
             
 setTimeout("location.href = 'https://proyectvendxp.luiscraftyt3.repl.co/process_recog';", 1500);
                 </script>
-<footer>Created and Built by: Luis-Net (Luis9799) https://luisweb.cf</footer>
+<footer>Created and Built with 💖 by: Luis-Net (Luis9799) https://luisweb.cf</footer>
 <a href="\return">Return to main menu.</a>
